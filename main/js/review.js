74300cdaf4c6a45813951e7ab1f21efbceced612
suminKim <kimtnals427@gmail.com>
2023-07-04 21:02:53 +0900

    230704_2100 메모별 개별 삭제 버튼 js 완료
    1.체크박스 클릭으로 개별 및 다중 삭제 작업하려고 했음
    2. 생성된 li에 대해서 js가 작동을 안함
    3. html에 미리 세팅해두고 세팅된 li에 대해서 삭제js 짰음
    4. 삭제자체를 모듈화 시키고 시간이 남으면 체크박스 다중삭제도 해보기

--- main/js/review.js
+++ main/js/review.js
@@ -1,5 +1,4 @@
 let makeNewMemo = document.querySelector('#makeNewMemo');
-let deleteMemo = document.querySelector('#deleteMemo');
 let memoDate = document.querySelectorAll("ul.memoDate");
 let checks = document.querySelectorAll('input.checkbox');
 
@@ -7,21 +6,28 @@ let checks = document.querySelectorAll('input.checkbox');
 makeNewMemo.addEventListener("click", function(){
     let makeLi = document.createElement('li');
 
-    makeLi.innerHTML = '<input type="checkbox" class="checkbox"> <input type="text">';
+    makeLi.innerHTML = '<input type="checkbox" class="checkbox"> <input type="text"><button class="saveMemo">저장</button><button class="deleteMemo">삭제</button>';
     
     memoDate[0].appendChild(makeLi)
+    //해당사항 저장하고 새로고침하여 보여지게 되어야함
 })
 
-for(let check of checks){
-    check.addEventListener('click', function(){
-        console.log("work?")
-        if(check.checked === 'true'){
-            console.log('plz')
-        }
+
+//체크박스 다중삭제는 나중에 고려해보기
+// for(let check of checks){
+//     check.addEventListener('click', function(){
+//         console.log("work?")
+//         if(check.checked === 'true'){
+//             console.log('plz')
+//         }
+//     })
+// }
+
+//삭제버튼시 해당 li 삭제됨
+let deleteBtns = document.querySelectorAll("li button.deleteMemo");
+for(let deleteBtn of deleteBtns){
+    deleteBtn.addEventListener('click', () => {
+        deleteBtn.parentNode.remove()
     })
 }
-deleteMemo.addEventListener("click", function(){
-    let delLi = document.querySelectorAll('li');
-    memoDate[0].removeChild(delLi[0])
-    
-})
+
